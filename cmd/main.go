531c965bb06960f211a8713040d01759ebe1bc2c
Joshua Zhang <toruta39@gmail.com>
2020-05-03 22:08:05 +0900

    check per 30sec

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
+	"time"
+
 	"github.com/sirupsen/logrus"
 	"github.com/toruta39/remofwd"
 )
 
-func main() {
+var client remofwd.Client
+var forwarder remofwd.Forwarder
+
+func Probe() {
 	logrus.Info("Start probing")
 
-	c := remofwd.NewClient()
-	f := remofwd.NewForwarder()
-	devices, err := c.FetchDevices()
+	devices, err := client.FetchDevices()
 	if err != nil {
 		panic(err)
 	}
 
 	for _, d := range devices {
-		_ = f.SendEvent(d.Name, d.Temperature())
-		_ = f.SendEvent(d.Name, d.Motion())
-		_ = f.SendEvent(d.Name, d.Humidity())
-		_ = f.SendEvent(d.Name, d.Illumination())
+		_ = forwarder.SendEvent(d.Name, d.Temperature())
+		_ = forwarder.SendEvent(d.Name, d.Motion())
+		_ = forwarder.SendEvent(d.Name, d.Humidity())
+		_ = forwarder.SendEvent(d.Name, d.Illumination())
 	}
 
 	logrus.Info("Finish probing")
+}
+
+func main() {
+	client = remofwd.NewClient()
+	forwarder = remofwd.NewForwarder()
+
+	for true {
+		Probe()
+		time.Sleep(30 * time.Second)
+	}
 }
